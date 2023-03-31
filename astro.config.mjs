67b9122899d0e9965d4e9e1e0e38fce104584cd9
Walther Seidel <waseidel@gmail.com>
2023-03-31 15:38:04 -0500

    fixing base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://waseidel.github.io',
-  base: '/waseidel.github.io'
+  base: '/'
 });
