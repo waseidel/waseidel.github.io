098451cfe9cf51b431f7dcf80854cda222dd9134
Walther Seidel <waseidel@gmail.com>
2023-11-23 14:15:34 -0500

    adding new configuration for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://waseidel.github.io',
-  base: '/'
+  base: '/',
+  integrations: [tailwind(), react()]
 });
