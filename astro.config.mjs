d628681270559b6772db83aa7641854bbde317ca
Walther Seidel <waseidel@gmail.com>
2023-11-27 14:06:03 -0500

    Testing index and react icons version 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://waseidel.github.io',
-  base: '/',
-  integrations: [tailwind(), react()]
+  site: "https://waseidel.github.io",
+  base: "/",
+  vite: {
+    ssr: {
+      noExternal: ["react-icons"],
+    },
+  },
+  integrations: [tailwind(), react()],
 });
+
